652e79ef137f15019b2ccd0b662dd703109db98f
petrosky <1aIIbXXufd77ceKCyktU>
2019-06-18 12:58:13 +1000

    26:15 into vid 7

--- users/views.py
+++ users/views.py
@@ -14,3 +14,9 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+def profile(request):
+    return render(request, 'users/profile.html')
+
+
